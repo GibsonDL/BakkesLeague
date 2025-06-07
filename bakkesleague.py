78cc569354ac6771915bebc09e493eff23155303
GibsonDL <145815996+GibsonDL@users.noreply.github.com>
2025-06-07 13:24:30 +0000

    simple code

---
+++ bakkesleague.py
@@ -0,0 +1,11 @@
+import os
+
+
+bakkesMod = "C:\\Program Files\\BakkesMod\\BakkesMod.exe"
+rocketLeague = "C:\\Users\\glumi\\Desktop\\Rocket League®.url"
+runBakkes = os.startfile(bakkesMod)
+runRocketLeague = os.startfile(rocketLeague)
+
+def open_app_os():
+    print(runBakkes)
+    print(runRocketLeague)
